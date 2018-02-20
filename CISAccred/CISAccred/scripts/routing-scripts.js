d7d0df5903f374c5991174064cdb4a7e1641fd03
die4700 <die4700@calu.edu>
2018-02-20 14:56:17 -0500

    Added some route providors and controllers

--- CISAccred/CISAccred/scripts/routing-scripts.js
+++ CISAccred/CISAccred/scripts/routing-scripts.js
@@ -1,14 +1,61 @@
 ﻿var cissacred = angular.module('cissacred', ["ngRoute"]);
 
-cissacred.config(function ($routeProvider)
-{
+cissacred.config(function ($routeProvider) {
     $routeProvider
         .when("/home", {
             templateUrl: "pages/home.html",
             controller: "mainController"
         })
+    $routeProvider
+        .when("/reports1", {
+            templateUrl: "/pages/reports/reports1.html",
+            controller: "reportsController"
+        });
+
+    $routeProvider
+        .when("/reports2", {
+            templateUrl: "/pages/reports/reports2.html",
+            controller: "reportsController"
+        });
+
+    $routeProvider
+        .when("/students", {
+            templateUrl: "/pages/students/students.html",
+            controller: "studentsController"
+        });
+
+    $routeProvider
+        .when("/students/addStudent", {
+            templateUrl: "/pages/students/addStudent.html",
+            controller: "studentsController"
+        });
+
+    $routeProvider
+        .when("/students/deleteStudent", {
+            templateUrl: "/pages/students/deleteStudent.html",
+            controller: "studentsController"
+        });
+
+    $routeProvider
+        .when("/students/editStudent", {
+            templateUrl: "/pages/students/editStudent.html",
+            controller: "studentsController"
+        });
+
 });
 
 cissacred.controller("mainController", function ($scope) {
     $scope.message = "Welcome to the home page.";
+});
+
+cisaccred.controller("loginController", function ($scope) {
+    $scope.message = "Welcome to the login page"
+});
+
+cisaccred.controller("reportsController", function ($scope) {
+    $scope.message = "Report pages";
+});
+
+cisaccred.controller("studentsController", function ($scope) {
+    $scope.message = "Student pages";
 });
