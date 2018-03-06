85626b3b848fc4083e8ad130f22d0a6d93802315
die4700 <die4700@calu.edu>
2018-03-06 09:46:31 -0500

    Commit to merge

--- CISAccred/CISAccred/scripts/routing-scripts.js
+++ CISAccred/CISAccred/scripts/routing-scripts.js
@@ -1,61 +1,77 @@
 ﻿var cissacred = angular.module('cissacred', ["ngRoute"]);
 
-cissacred.config(function ($routeProvider) {
+cissacred.config(function ($routeProvider)
+{
     $routeProvider
-        .when("/home", {
+        .when("/home",
+        {
             templateUrl: "pages/home.html",
             controller: "mainController"
         })
     $routeProvider
-        .when("/reports1", {
+        .when("/reports1",
+        {
             templateUrl: "/pages/reports/reports1.html",
             controller: "reportsController"
         });
 
     $routeProvider
-        .when("/reports2", {
+        .when("/reports2",
+        {
             templateUrl: "/pages/reports/reports2.html",
             controller: "reportsController"
         });
 
     $routeProvider
-        .when("/students", {
+        .when("/students",
+        {
             templateUrl: "/pages/students/students.html",
             controller: "studentsController"
         });
 
     $routeProvider
-        .when("/students/addStudent", {
+        .when("/students/addStudent",
+        {
             templateUrl: "/pages/students/addStudent.html",
             controller: "studentsController"
         });
 
     $routeProvider
-        .when("/students/deleteStudent", {
+        .when("/students/deleteStudent",
+        {
             templateUrl: "/pages/students/deleteStudent.html",
             controller: "studentsController"
         });
 
     $routeProvider
-        .when("/students/editStudent", {
+        .when("/students/editStudent",
+        {
             templateUrl: "/pages/students/editStudent.html",
             controller: "studentsController"
         });
 
 });
 
-cissacred.controller("mainController", function ($scope) {
+cissacred.controller("mainController", function ($scope)
+{
     $scope.message = "Welcome to the home page.";
 });
 
-cisaccred.controller("loginController", function ($scope) {
+cisaccred.controller("loginController", function ($scope)
+{
     $scope.message = "Welcome to the login page"
+    $scope.formSubmit = function()
+    {
+
+    }
 });
 
-cisaccred.controller("reportsController", function ($scope) {
+cisaccred.controller("reportsController", function ($scope)
+{
     $scope.message = "Report pages";
 });
 
-cisaccred.controller("studentsController", function ($scope) {
+cisaccred.controller("studentsController", function ($scope)
+{
     $scope.message = "Student pages";
 });
