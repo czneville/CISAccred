234e477b10c32e5b4274add3ead3fe2fd43324fd
die4700 <die4700@calu.edu>
2018-03-29 14:00:36 -0400

    Merge because I want the new stuff.

--- CISAccred/CISAccred/angular/abet.controller.js
+++ CISAccred/CISAccred/angular/abet.controller.js
@@ -2,4 +2,32 @@
 
 CISAccredApp.controller('abetController', function ($scope) {
     $scope.message = 'You\'re on the ABET data page!';
+
+    $scope.addClass = function () {
+        $.notify("addClass");
+    }
+
+    $scope.addCriteria = function () {
+
+    }
+
+    $scope.addObjective = function () {
+
+    }
+
+    $scope.addOutcome = function () {
+
+    }
+
+    $scope.addRubric = function () {
+
+    }
+
+    $scope.addScoreset = function () {
+
+    }
+
+    $scope.addTask = function () {
+
+    }
 });
