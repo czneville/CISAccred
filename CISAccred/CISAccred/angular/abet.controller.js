45800f0d888e41c24cfcaf1b755522b4c0fc6fa1
die4700 <die4700@calu.edu>
2018-03-29 22:11:17 -0400

    WIP for cis_class, don't know how to fix.

--- CISAccred/CISAccred/angular/abet.controller.js
+++ CISAccred/CISAccred/angular/abet.controller.js
@@ -1,11 +1,18 @@
 ﻿var CISAccredApp = angular.module('CISAccredApp');
 
-CISAccredApp.controller('abetController', function ($scope, session) {
+CISAccredApp.controller('abetController', function ($scope, session, php) {
     session.showLoginForm();
     $scope.message = 'You\'re on the ABET data page!';
 
     $scope.addClass = function () {
-        $.notify("addClass");
+        var classData = Array();
+        classData["c_semester"] = c_semester;
+        classData["c_course_num"] = c_course_num;
+        classData["c_section"] = c_section;
+        php.post(classData, url, function (response) {});
+
+        var url = '/api/addClass.php';
+
     }
 
     $scope.addCriteria = function () {
---
+++ CISAccred/CISAccred/api/addClass.php
@@ -0,0 +1,11 @@
+﻿<?php
+	$c_semester = $)POST['c_semester'];
+	$c_course_num = $)POST['c_course_num'];
+	$c_section = $)POST['c_section'];
+	include 'oracleQuery.php'
+	$query = executeQuery('insert into c_class values ('.$c_semester.', .$c_course_num., .$c_section.) ');
+	if(!isset($query[0])){
+    echo("{}");
+    exit;
+  }
+?>
