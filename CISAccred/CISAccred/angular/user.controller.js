50205886af6b076d2ffd09729e2089aecbd73987
die4700 <die4700@calu.edu>
2018-04-05 14:15:24 -0400

    Added validate.js, comments to addClass.php

--- CISAccred/CISAccred/angular/user.controller.js
+++ CISAccred/CISAccred/angular/user.controller.js
@@ -12,4 +12,5 @@ CISAccredApp.controller('userController', function ($scope, session) {
         userData[""] = p_password;
         userData[""] = p_isadmin;
 
+    }
 });
--- CISAccred/CISAccred/api/addClass.php
+++ CISAccred/CISAccred/api/addClass.php
@@ -4,6 +4,12 @@
 	$c_section = $_POST['section'];
 	include 'oracleQuery.php';
 	$query = executeQuery("INSERT INTO CIS_CLASS VALUES('".$c_semester."', ".$c_course_num.", ".$c_section.")",false);
+	/*
+	* If query was select query, $query contains array of results.
+	* Else query is true / false if successful.
+	* If false, sends out 500 header.
+	*/
+	
 	if($query){
     exit();
   }else{
